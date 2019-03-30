0058da89bb30453c20d6ecc4ef29ba797ccfd40c
gyf1214 <gyf1214@gmail.com>
2019-03-30 13:44:12 +0800

    change text script

--- example/tiny/main.go
+++ example/tiny/main.go
@@ -1,14 +1,9 @@
 package main
 
 import (
-	"bytes"
-	"crypto/tls"
-	"io"
 	"log"
 	"net/http"
-	"sync"
 
-	"github.com/lucas-clemente/quic-go"
 	"github.com/lucas-clemente/quic-go/h2quic"
 	"github.com/lucas-clemente/quic-go/internal/utils"
 )
@@ -24,38 +19,12 @@ func handle(resp http.ResponseWriter, req *http.Request) {
 	}
 }
 
-func startServer(wg *sync.WaitGroup) {
+func startServer() {
 	http.HandleFunc("/", handle)
 	h2quic.ListenAndServe(":6121", "example/fullchain.pem", "example/privkey.pem", nil)
-	wg.Add(1)
-}
-
-func testRequest() {
-	client := &http.Client{
-		Transport: &h2quic.RoundTripper{
-			QuicConfig:      &quic.Config{CreatePaths: true},
-			TLSClientConfig: &tls.Config{InsecureSkipVerify: true},
-		},
-	}
-	resp, err := client.Get("https://127.0.0.1:6121")
-	if err != nil {
-		log.Fatal(err)
-	}
-	body := &bytes.Buffer{}
-	_, err = io.Copy(body, resp.Body)
-	if err != nil {
-		log.Fatal(err)
-	}
-	log.Println(body.Len())
 }
 
 func main() {
 	utils.SetLogLevel(utils.LogLevelInfo)
-
-	wg := &sync.WaitGroup{}
-	go startServer(wg)
-
-	testRequest()
-
-	wg.Wait()
+	startServer()
 }
